3e0c0eb7f52328f00baada866915fefde4145246
davisantosf <160255907+davisantosf@users.noreply.github.com>
2024-08-12 10:49:51 -0300

    Add files via upload

---
+++ aula 7 - projeto 2/js/perguntas.js
@@ -0,0 +1,86 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia que pode ajudar a visualizar dados e criar representações do desmatamento na Amazônia. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é preocupante!",
+                afirmacao: ["Ficou preocupado com a possibilidade de a tecnologia ser usada para monitorar, mas também para intensificar o desmatamento.",
+                    "Achou inquietante pensar na quantidade de dados sobre o desmatamento que podem ser manipulados por essa tecnologia."
+                ]
+            },
+            {
+                texto: "Isso é uma grande oportunidade!",
+                afirmacao: ["Quis saber como a tecnologia poderia ajudar a monitorar e combater o desmatamento.",
+                    "Foi atrás de ferramentas e informações sobre como usar a tecnologia para apoiar a preservação da Amazônia."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de biologia da escola decidiu fazer uma sequência de aulas sobre o uso da IA para o monitoramento ambiental. No fim de uma aula, ela pede que você escreva um trabalho sobre como a IA pode ser usada para combater o desmatamento na Amazônia. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para encontrar informações relevantes sobre o combate ao desmatamento e explicar numa linguagem que facilite o entendimento.",
+                afirmacao: ["Conseguiu utilizar a IA para buscar informações úteis sobre o desmatamento e estratégias de combate.",
+                    "Percebeu que a IA pode ajudar a encontrar dados e soluções mais rápidas e direcionadas para questões ambientais.",
+                    "Viu que a IA consegue explicar termos técnicos e dados complexos de forma mais acessível e isso ajudou muito sua pesquisa sobre o desmatamento."
+                ]
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: ["Sentiu mais confiança em utilizar seus próprios conhecimentos e pesquisas para escrever o trabalho.",
+                    "Achou mais fácil buscar informações usando métodos tradicionais, mesmo que demorasse mais.",
+                    "Teve algumas reservas sobre o uso de IA e preferiu evitar o uso excessivo da tecnologia em favor de abordagens mais tradicionais."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate sobre o impacto da tecnologia na conservação ambiental e como a IA pode influenciar o futuro das iniciativas de proteção. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades para monitorar e proteger a Amazônia e melhorar estratégias de conservação.",
+                afirmacao: ["Acredita que a IA pode impulsionar inovações na conservação ambiental e ajudar a criar novas abordagens para proteger a Amazônia.",
+                    "Participa ativamente de projetos que usam IA para monitoramento e desenvolvimento de soluções de proteção ambiental."
+                ]
+            },
+            {
+                texto: "Me preocupo com a possibilidade de que a tecnologia possa ser usada de maneira inadequada e defendo a importância de garantir que a IA seja usada eticamente na conservação.",
+                afirmacao: ["Sua preocupação motivou a criação de um grupo de estudos para discutir o uso ético da IA na conservação da Amazônia.",
+                    "Criou grupos focados em ética ambiental e busca reduzir os riscos associados ao uso de IA na proteção da floresta."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse suas ideias sobre o impacto da IA no combate ao desmatamento. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: ["Notou que muitas pessoas ainda têm dificuldades com ferramentas tradicionais e decidiu compartilhar seus conhecimentos sobre design digital com iniciantes.",
+                    "Ainda acredita que técnicas tradicionais de arte são melhores para expressar ideias sobre a conservação e está incentivando outros a utilizar métodos como pintura manual."
+                ]
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: ["Aproveitou a tecnologia para criar rapidamente imagens que ajudam a comunicar ideias sobre a conservação da Amazônia e agora ensina outros a usar essas ferramentas.",
+                    "Compartilhou as artes geradas em redes sociais para aumentar a conscientização sobre o desmatamento e como a IA pode ajudar na visualização de dados ambientais.",
+                    "Percebeu que muitos têm dificuldade em criar representações visuais e acredita que a IA pode empoderar essas pessoas a expressar suas ideias de forma mais eficaz."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia sobre o desmatamento da Amazônia para entregar na semana seguinte. O andamento está um pouco atrasado e uma pessoa do seu grupo decidiu usar a IA para gerar o conteúdo. O problema é que o trabalho está muito parecido com o texto gerado pelo chat. O que você faz?",
+        alternativas: [
+            {
+                texto: "Escrever comandos para a IA é uma forma válida de contribuição, então usar o texto gerado é aceitável.",
+                afirmacao: "Infelizmente, passou a confiar demais na IA para tarefas acadêmicas e sente-se dependente da tecnologia para concluir trabalhos."
+            },
+            {
+                texto: "Embora a IA seja uma ferramenta avançada, é essencial revisar o trabalho e adicionar perspectivas pessoais para garantir a originalidade e a precisão.",
+                afirmacao: "Percebeu que os textos gerados pela IA são baseados em padrões e pode não refletir completamente suas próprias ideias, então entende que a IA deve ser usada como apoio, e não como substituto final."
+            }
+        ]
+    },
+];
